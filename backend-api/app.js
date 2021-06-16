7b72362239c1c8f031407ee15fa7b45b28322be0
Rafael <rafaelpupio@gmail.com>
2021-06-15 22:08:36 -0400

    backend Mongodb

---
+++ backend-api/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// APP IS RUNNING AT PORT 5000
+var app = express();
+
+
+// MONGO CONNECTION
+var connectionString = " mongodb://127.0.0.1/express-api";
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(){
+    console.log("database is connected");
+})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
